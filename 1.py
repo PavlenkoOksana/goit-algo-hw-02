97c78191de276f20d3979c3f75dade10e0b9292f
PavlenkoOksana <pavlenko.oksana.od@gmail.com>
2023-12-21 15:16:14 +0100

    21.12.2023

--- 1.py
+++ 1.py
@@ -1,22 +1,15 @@
 from queue import Queue
-import random
 
 
-# Створити чергу заявок
 queue = Queue()
-
-# Функція generate_request(): Створити нову заявку та Додати заявку до черги
+num_app = 1
 
 def generate_request():
-    new_application = random.randint(1, 100)
-    queue.put(new_application)
-    print (f"Надійшла нова заявка № {new_application} у роботу")
+    global num_app
+    queue.put(num_app)
+    print (f"Надійшла нова заявка № {num_app} у роботу")
+    num_app += 1
 
-# Функція process_request(): Якщо черга не пуста:
-    #     Видалити заявку з черги
-    #     Обробити заявку
-    # Інакше:
-    #     Вивести повідомлення, що черга пуста
 
 def process_request():
     if not queue.empty():
@@ -25,16 +18,11 @@ def process_request():
     else:
       print("Черга заявок порожня.")
 
-# Головний цикл програми:
-#     Поки користувач не вийде з програми:
-#         Виконати generate_request() для створення нових заявок
-#         Виконати process_request() для обробки заявокuser_command = input("Введіть команду, для виходу виконайте end:")
-
-user_command = input("Введіть команду, для виходу введіть end: \n")
-while user_command != "end":
-    generate_request()
-    process_request()
-    user_command = input("Введіть команду, для виходу введіть end: \n")
-
-print ("Програма завершила виконання")
+try:
+    while True:
+        generate_request()
+        process_request()
+except KeyboardInterrupt:
+    print("\nПрограма була перервана користувачем.")
+    print("Програма завершила виконання.")
 
